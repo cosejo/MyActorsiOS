ec7be0d46347c29f8b7e1d2da1c42775d1d43e18
Carlos Osejo <caosejo@gmail.com>
2021-07-22 11:43:16 -0600

    Create the project

---
+++ Shared/MyActorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyActorsApp.swift
+//  Shared
+//
+//  Created by Carlos Osejo on 21/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MyActorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
